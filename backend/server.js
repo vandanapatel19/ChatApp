9aeda8b42747220f02395da926e15dc1e0d9953a
Vandana Patel <vandana_cse_2023@rkgitm.ac.in>
2026-09-12 23:44:38 +0530

    updation

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import "dotenv/config.js";
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import UserRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 
 //create express app and http server
 const app = express();
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 //initialize socket.io server
 export const io = new Server(server, {
-    cors:{origin:"*"}
+  cors: { origin: "*" }
 })
 
 //Store online users
@@ -21,30 +21,30 @@ export const userSocketMap = {}; // {userId: socketId}
 
 //Socket.io connection handler
 io.on("connection", (socket) => {
-      const userId = socket.handshake.query.userId;
-      console.log(`User connected: ${userId}`);
+  const userId = socket.handshake.query.userId;
+  console.log(`User connected: ${userId}`);
 
-      if (userId) {
-        userSocketMap[userId] = socket.id;
-      }
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
 
-      //Emit online users to all connected clients
-      io.emit("onlineUsers", Object.keys(userSocketMap));
+  //Emit online users to all connected clients
+  io.emit("onlineUsers", Object.keys(userSocketMap));
 
-        //Handle disconnection
-        socket.on("disconnect", () => {
-            console.log("User disconnected", userId);
-            delete userSocketMap[userId];
-            io.emit("onlineUsers", Object.keys(userSocketMap));
-        })
+  //Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("onlineUsers", Object.keys(userSocketMap));
+  })
 })
 
-
 //middlewares
-app.use(express.json({limit: "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 //routes
+app.get('/', (req, res) =>  res.send("Server is Live!") )
 app.use('/api/status', (req, res) => res.send("server is live"));
 app.use('/api/auth', UserRouter);
 app.use('/api/messages', messageRouter);
@@ -52,6 +52,9 @@ app.use('/api/messages', messageRouter);
 //Database connection
 await connectDB();
 
-const PORT = process.env.PORT || 7000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 7000;
+  server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+}
 
-server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+export default server;
